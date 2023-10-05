47775072e70fadc89effa18450433f766aae5f52
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-05 17:33:41 -0300

    change student class to receive a classroom

--- Project/student.rb
+++ Project/student.rb
@@ -1,6 +1,9 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
@@ -8,5 +11,10 @@ class Student < Person
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.add_student(self)
   end
 end
