b3553e2ccf284884060314b7e3b297f3b008a234
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-05 21:07:52 -0300

    create app.rb

--- Project/student.rb
+++ Project/student.rb
@@ -4,7 +4,7 @@ require_relative 'classroom'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true, classroom: nil)
     super(age, name, parent_permission)
     @classroom = classroom
   end
---
+++ project/app.rb
@@ -0,0 +1,91 @@
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'book'
+require_relative 'rental'
+
+class App
+  def initialize
+    @students = []
+    @teachers = []
+    @books = []
+    @rentals = []
+  end
+
+  def list_books
+    @books.each do |book|
+      puts book
+    end
+  end
+
+  def list_people
+    @students.each do |student|
+      puts student
+    end
+    @teachers.each do |teacher|
+      puts teacher
+    end
+  end
+
+  def create_student
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Has parent permission? (Y/N): '
+    permission = gets.chomp.downcase == 'y'
+    @students << if permission
+                   Student.new(age, name)
+                 else
+                   Student.new(age, name, false)
+                 end
+  end
+
+  def create_teacher
+    print 'Age: '
+    age = gets.chomp.to_i
+    print 'Name: '
+    name = gets.chomp
+    print 'Specialization: '
+    specialization = gets.chomp
+    @teachers << Teacher.new(age, specialization, name)
+  end
+
+  def create_person
+    puts 'Do you want to create a student (1) or a teacher (2)?'
+    person_type = gets.chomp.to_i
+    case person_type
+    when 1
+      create_student
+    when 2
+      create_teacher
+    else
+      puts 'Person type not recognized'
+    end
+  end
+
+  def create_book
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    @books << Book.new(title, author)
+  end
+
+  def create_rental
+    puts 'Select a book from the following list by number'
+    list_books
+    book_index = gets.chomp.to_i - 1
+    puts 'Select a student from the following list by number (not id)'
+    list_people
+    person_index = gets.chomp.to_i - 1
+    puts 'Date: '
+    date = gets.chomp
+    @rentals << Rental.new(date, @books[book_index], @people[person_index])
+  end
+
+  def list_rentals
+    @rentals.each do |rental|
+      puts rental
+    end
+  end
+end
