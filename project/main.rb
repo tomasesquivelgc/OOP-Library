fb800f9d295753b54330e2acfd92a2cfb8b29e70
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-05 23:28:13 -0300

    fix cyclomatic complexity

--- project/main.rb
+++ project/main.rb
@@ -1,39 +1,33 @@
 require_relative 'app'
 
-def list_options
-  puts 'Please choose an option by entering a number:'
-  puts '1 - Create a person'
-  puts '2 - Create a book'
-  puts '3 - Create a rental'
-  puts '4 - List all books'
-  puts '5 - List all people'
-  puts '6 - List all rentals'
-  puts '7 - Exit'
+def handle_option(app, option)
+  case option
+  when 1 then app.create_person
+  when 2 then app.create_book
+  when 3 then app.create_rental
+  when 4 then app.list_books
+  when 5 then app.list_people
+  when 6 then app.list_rentals
+  when 7 then exit
+  else
+    puts 'Invalid option. Try again.'
+  end
 end
 
 def main
   app = App.new
   loop do
-    list_options
+    puts 'Please choose an option by entering a number:'
+    puts '1 - Create a person'
+    puts '2 - Create a book'
+    puts '3 - Create a rental'
+    puts '4 - List all books'
+    puts '5 - List all people'
+    puts '6 - List all rentals'
+    puts '7 - Exit'
+
     option = gets.chomp.to_i
-    case option
-    when 1
-      app.create_person
-    when 2
-      app.create_book
-    when 3
-      app.create_rental
-    when 4
-      app.list_books
-    when 5
-      app.list_people
-    when 6
-      app.list_rentals
-    when 7
-      break
-    else
-      puts 'Invalid option. Try again.'
-    end
+    handle_option(app, option)
   end
 end
 
