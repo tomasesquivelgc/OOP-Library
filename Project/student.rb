b2c9f27e66d21e3b898c8629b2e9c5aa3d9d87d6
Tomas Esquivel <tomasesquivel@yahoo.com>
2023-10-05 22:00:14 -0300

    fix student creator

--- Project/student.rb
+++ Project/student.rb
@@ -5,7 +5,7 @@ class Student < Person
   attr_reader :classroom
 
   def initialize(age, name = 'Unknown', parent_permission: true, classroom: nil)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
