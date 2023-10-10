f613b03553b76db9d2db8dbed0c18af7daabdbcd
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-09 20:06:06 -0500

    Correct the order of the displayed menu options

--- Project/main.rb
+++ Project/main.rb
@@ -5,23 +5,23 @@ class Menu
   end
 
   def display_options
-    puts 'Please choose an option by entering a number:'
-    puts '1 - Create a person'
-    puts '2 - Create a book'
-    puts '3 - Create a rental'
-    puts '4 - List all books'
-    puts '5 - List all people'
-    puts '6 - List all rentals'
+    puts "\nPlease  choose an option by entering a number:"
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
     puts '7 - Exit'
   end
 
   def handle_option(option)
     case option
-    when 1 then @app.create_person
-    when 2 then @app.create_book
-    when 3 then @app.create_rental
-    when 4 then @app.list_books
-    when 5 then @app.list_people
+    when 1 then @app.list_books
+    when 2 then @app.list_people
+    when 3 then @app.create_person
+    when 4 then @app.create_book
+    when 5 then @app.create_rental
     when 6 then @app.list_rentals
     when 7 then exit
     else
