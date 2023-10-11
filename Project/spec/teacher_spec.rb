fc00d155d8232211da75723b9a9b9cfd3f31fc02
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-10-11 17:15:30 -0500

    Create test to check the new Teacher creation

---
+++ Project/spec/teacher_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../teacher'
+
+describe Teacher do
+  describe '#initialize' do
+    it 'creates a new teacher with the age, specialization and name attributes' do
+      teacher = Teacher.new(30, 'Math', 'Jim')
+
+      expect(teacher.age).to eq(30)
+      expect(teacher.specialization).to eq('Math')
+      expect(teacher.name).to eq('Jim')
+      expect(teacher.parent_permission).to be(true)
+      expect(teacher.rental).to be_empty
+    end
+  end
+end
+
